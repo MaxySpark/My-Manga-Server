4abf277cfe42679528d12ed4eca029a45756f389
MaxySpark <maxyspark@gmail.com>
2017-01-23 19:11:23 +0530

    init

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const engines = require('consolidate');
+const app = express();
+const fs = require('fs');
+
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + "/views");
+app.use(express.static('img'));
+var images = 0;
+var imgarr =[];
+app.get('/',function(req, res){
+    res.render('index');
+});
+fs.readdir('./img/',function(err, files){
+   if (err) {
+      return console.error(err);
+   }
+   console.log(files.length);
+   images = files.length;
+   for(var i = 1;i<=images;i++) {
+       imgarr.push(i+".jpg");
+   }
+   app.get('/manga',function(req, res){
+    res.status(200);
+    res.render('manga',{"images":imgarr});
+});
+});
+app.listen(3000, function() {
+    console.log("Server running at port 3000!");
+});

13c2717e27c84715cca20814bcffb62a18e052b0
MaxySpark <maxyspark@gmail.com>
2017-02-15 00:48:30 +0530

    refactoring manga server for v2

--- app.js
+++ app.js
@@ -6,26 +6,44 @@ const fs = require('fs');
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + "/views");
-app.use(express.static('img'));
+app.use(express.static('files'));
 var images = 0;
 var imgarr =[];
+
 app.get('/',function(req, res){
     res.render('index');
 });
-fs.readdir('./img/',function(err, files){
-   if (err) {
-      return console.error(err);
-   }
-   console.log(files.length);
-   images = files.length;
-   for(var i = 1;i<=images;i++) {
-       imgarr.push(i+".jpg");
-   }
-   app.get('/manga',function(req, res){
-    res.status(200);
-    res.render('manga',{"images":imgarr});
+
+app.get('/online',function(req, res){
+    res.render('index');
+    var search = req.query.search;
+    if(search){
+        console.log(search);
+    }
 });
+
+app.get('/offline',function(req, res){
+    
+    res.send(200);
 });
+
+
+// fs.readdir('./img/',function(err, files){
+//    if (err) {
+//       return console.error(err);
+//    }
+//    console.log(files.length);
+//    images = files.length;
+//    for(var i = 1;i<=images;i++) {
+//        imgarr.push(i+".jpg");
+//    }
+//    app.get('/manga',function(req, res){
+//     res.status(200);
+//     res.render('manga',{"images":imgarr});
+// });
+// });
+
+
 app.listen(3000, function() {
     console.log("Server running at port 3000!");
 });

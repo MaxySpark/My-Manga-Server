21ead4fee5f33b196617a60f295166ec8b972df2
SIKHAR SAIKIA <maxyspark@gmail.com>
2017-02-16 03:31:48 +0530

    added read folder

--- app.js
+++ app.js
@@ -5,87 +5,104 @@ const fs = require('fs');
 const path = require('path');
 const getChapter = require('./lib/read_chapter');
 const manga = require('./lib/manga');
+const readDir = require('./lib/readfile');
+
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + "/views");
 app.use(express.static('files'));
 app.use('/ext', express.static(path.join(__dirname, 'views/ext')));
 
-app.get('/',function(req, res){
-    res.render('index');
-});
+//folder read begins
+var folderList = [];
+readDir(folderUpdate);
 
-app.use('/online',function(req, res, next) {
-    var search = req.query.search;
-    var chap = req.query.chap;
-    var chap_to = req.query.chap_to;    
-    if(search){
-        console.log(search);
-        manga(search,chap,chap_to,next);
-    }
-})
+function folderUpdate(folders){
+    folderList = folders;
+    // console.log(folderList);
+}
+//folder read ends
 
-app.get('/online',function(req, res){
-    res.render('online');
-});
+//setTimeout
+setTimeout(function() {
 
-
-fs.readdir('./files/',function(err, files){
-    var fileObj = [];
-    var inc = 0;
-    if (err) {
-        return console.error(err);
-    }
-    console.log(files);
-    files.forEach(function(file){
-        fileObj.push({
-            "name": file,
-            "id" : ++inc
-        });
-    });
-    console.log(fileObj);
-    app.get('/offline',function(req, res){
-            res.status(200);
-            res.render('offline',{"dirList":fileObj});
+    app.get('/',function(req, res){
+        res.render('index');
     });
 
-    app.get('/offline/:id',function(req, res){
-        var id = parseInt(req.params.id,10)-1;
-        console.log(id);
-        var chapterList = getChapter(files[id]);
-        var chapObj = [];
-        var inc = 0;        
-        chapterList.forEach(function(element){
-            chapObj.push({
-                "name" : element,
+    app.use('/online',function(req, res, next) {
+        var search = req.query.search;
+        var chap = req.query.chap;
+        var chap_to = req.query.chap_to;    
+        if(search){
+            console.log(search);
+            manga(search,chap,chap_to,next);
+        }
+    })
+
+    app.get('/online',function(req, res){
+        res.render('online');
+    });
+
+
+    fs.readdir('./files/',function(err, files){
+        var fileObj = [];
+        var inc = 0;
+        if (err) {
+            return console.error(err);
+        }
+        console.log(files);
+        files.forEach(function(file){
+            fileObj.push({
+                "name": file,
                 "id" : ++inc
             });
         });
-        res.render('chapter',{"chapterList":chapObj, "manga":files[id] });
+        console.log(fileObj);
+        app.get('/offline',function(req, res){
+                res.status(200);
+                res.render('offline',{"dirList":fileObj});
+        });
+
+        app.get('/offline/:id',function(req, res){
+            var id = parseInt(req.params.id,10)-1;
+            console.log(id);
+            var chapterList = getChapter(files[id]);
+            var chapObj = [];
+            var inc = 0;        
+            chapterList.forEach(function(element){
+                chapObj.push({
+                    "name" : element,
+                    "id" : ++inc
+                });
+            });
+            res.render('chapter',{"chapterList":chapObj, "manga":files[id] });
+        });
+
     });
 
-});
-
-app.get('/manga',function(req, res){
-        var manga = req.query.manga;
-        var chapter_name = req.query.chap_name;
-        var images = 0;
-        var imgarr = [];
-        fs.readdir(__dirname+'/files/'+manga+'/'+chapter_name+'/',function(err, files){
-            if (err) {
-                return console.error(err);
+    app.get('/manga',function(req, res){
+            var manga = req.query.manga;
+            var chapter_name = req.query.chap_name;
+            var images = 0;
+            var imgarr = [];
+            fs.readdir(__dirname+'/files/'+manga+'/'+chapter_name+'/',function(err, files){
+                if (err) {
+                    return console.error(err);
+                }
+                console.log(files);
+                images = files.length;
+            });
+            for(var i = 1;i<=images;i++) {
+                imgarr.push(i+".jpg");
             }
-            console.log(files);
-            images = files.length;
-        });
-        for(var i = 1;i<=images;i++) {
-            imgarr.push(i+".jpg");
-        }
 
-    res.render('manga',{"images":imgarr});
+        res.render('manga',{"images":imgarr});
 
-});
+    });
 
-app.listen(3000, function() {
-    console.log("Server running at port 3000!");
-});
+    app.listen(3000, function() {
+        console.log("Server running at port 3000!");
+    });
+
+},8000);
